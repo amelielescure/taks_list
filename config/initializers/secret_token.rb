4b1791d25ddca905a35997634360c66a4502bea4
amelielescure <amelieles78@gmail.com>
2016-10-11 21:22:09 +0200

    Init project tasks list

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TasksList::Application.config.secret_key_base = '2a56ab3d3226427b08aaffe2d623f6c5ca05dce4c29d19906901e266629ccd7f259e67ae0833e54c3bace79ade62225c4d7af93c191088df51d2c7cc08c09c0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TasksList::Application.config.session_store :cookie_store, key: '_tasks-list_session'
